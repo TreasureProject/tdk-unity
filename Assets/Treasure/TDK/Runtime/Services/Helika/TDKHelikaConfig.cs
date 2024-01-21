63b6163f1f983205a33973d01fd21081d0016a3b
Yuri Visser <yurivisser7@gmail.com>
2024-01-21 10:49:38 -0400

    feat: support environment & platform api keys for Helika

--- Assets/Treasure/TDK/Runtime/Services/Helika/TDKHelikaConfig.cs
+++ Assets/Treasure/TDK/Runtime/Services/Helika/TDKHelikaConfig.cs
@@ -5,30 +5,56 @@ namespace Treasure
 {
     public class TDKHelikaConfig : ScriptableObject
     {
-        [SerializeField] private string _apiKey = string.Empty;
-        [SerializeField] private string _env = string.Empty;
+        [SerializeField] public string _prodApiKeyWeb;
+        [SerializeField] public string _prodApiKeyIos;
+        [SerializeField] public string _prodApiKeyAndroid;
+        [SerializeField] public string _devApiKeyWeb;
+        [SerializeField] public string _devApiKeyIos;
+        [SerializeField] public string _devApiKeyAndroid;
 
         public string ApiKey
         {
-            get { return _apiKey; }
-        }
-
-        public string Env
-        {
-            get { return _env; }
+            get {
+                if(TDK.Instance.AppConfig.Environment.Equals("prod")) {
+                    #if UNITY_IOS
+                    return _prodApiKeyIos;
+                    #elif UNITY_ANDROID
+                    return _prodApiKeyAndroid;
+                    #else
+                    return _prodApiKeyWeb;
+                    #endif
+                }
+                else {
+                    #if UNITY_IOS
+                    return _devApiKeyIos;
+                    #elif UNITY_ANDROID
+                    return _devApiKeyAndroid;
+                    #else
+                    return _devApiKeyWeb;
+                    #endif
+                }
+            }
         }
 
         public void SetConfig(SerializedHelikaConfig config)
         {
-            _apiKey = config.apiKey;
-            _env = config.env;
+            _prodApiKeyWeb = config.prodApiKeyWeb;
+            _prodApiKeyIos = config.prodApiKeyIos;
+            _prodApiKeyAndroid = config.prodApiKeyAndroid;
+            _devApiKeyWeb = config.devApiKeyWeb;
+            _devApiKeyIos = config.devApiKeyIos;
+            _devApiKeyAndroid = config.devApiKeyAndroid;
         }
     }
 
     [Serializable]
     public class SerializedHelikaConfig
     {
-        [SerializeField] public string apiKey;
-        [SerializeField] public string env;
+        [SerializeField] public string prodApiKeyWeb;
+        [SerializeField] public string prodApiKeyIos;
+        [SerializeField] public string prodApiKeyAndroid;
+        [SerializeField] public string devApiKeyWeb;
+        [SerializeField] public string devApiKeyIos;
+        [SerializeField] public string devApiKeyAndroid;
     }
 }
