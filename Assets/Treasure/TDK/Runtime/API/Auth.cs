f7850cdb10f9604a420f41330454446a35909f3a
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-04 15:53:41 -0800

    Thirdweb v4.7.4 (#10)
    * updated thirdweb sdk to v4.7.4
    
    * update configs
    
    * move auth operations to use TDK API
    
    ---------
    
    Co-authored-by: Yuri Visser <yurivisser7@gmail.com>

---
+++ Assets/Treasure/TDK/Runtime/API/Auth.cs
@@ -0,0 +1,84 @@
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Treasure
+{
+    [Serializable]
+    public class AuthPayload
+    {
+        public string type;
+        public string domain;
+        public string address;
+        public string statement;
+        public string uri;
+        public string version;
+        public string chain_id;
+        public string nonce;
+        public string issued_at;
+        public string expiration_time;
+        public string invalid_before;
+
+        public AuthPayload()
+        {
+            type = "evm";
+        }
+    }
+
+    [Serializable]
+    internal class GetAuthPayloadBody
+    {
+        public string address;
+        public string chainId;
+    }
+
+    [Serializable]
+    public struct GetAuthPayloadResponse
+    {
+        public AuthPayload payload;
+    }
+
+    [Serializable]
+    public struct LogInBody
+    {
+        public struct Payload
+        {
+            public AuthPayload payload;
+            public string signature;
+        }
+
+        public Payload payload;
+    }
+
+    [Serializable]
+    public struct LogInResponse
+    {
+        public string token;
+    }
+
+    public partial class API
+    {
+        public async Task<AuthPayload> GetAuthPayload(string address, string chainId)
+        {
+            var response = await Post("/auth/payload", JsonConvert.SerializeObject(new GetAuthPayloadBody()
+            {
+                address = address,
+                chainId = chainId,
+            }));
+            return JsonConvert.DeserializeObject<GetAuthPayloadResponse>(response).payload;
+        }
+
+        public async Task<string> LogIn(AuthPayload payload, string signature)
+        {
+            var response = await Post("/auth/login", JsonConvert.SerializeObject(new LogInBody()
+            {
+                payload = new LogInBody.Payload()
+                {
+                    payload = payload,
+                    signature = signature,
+                }
+            }));
+            return JsonConvert.DeserializeObject<LogInResponse>(response).token;
+        }
+    }
+}
