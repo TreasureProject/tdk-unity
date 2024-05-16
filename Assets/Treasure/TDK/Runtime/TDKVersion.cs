959ed318ebfb4b965a5a47283cfb4d41895599be
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-05-16 13:56:08 -0700

    v0.4.0

--- Assets/Treasure/TDK/Runtime/TDKVersion.cs
+++ Assets/Treasure/TDK/Runtime/TDKVersion.cs
@@ -3,6 +3,6 @@
     public static class TDKVersion
     {
         public static string name = "tdk-unity";
-        public static string version = "0.3.2";
+        public static string version = "0.4.0";
     }
 }
