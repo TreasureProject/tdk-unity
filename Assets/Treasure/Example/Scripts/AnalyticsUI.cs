0363450659f00a380ed9d5ac71b0267705bbaff9
Yuri Visser <yurivisser7@gmail.com>
2024-05-27 16:29:41 -0300

    added additional tracking ui/methods

--- Assets/Treasure/Example/Scripts/AnalyticsUI.cs
+++ Assets/Treasure/Example/Scripts/AnalyticsUI.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class AnalyticsUI : MonoBehaviour
 {
+    public void OnTrackFtueStartedBtn()
+    {
+        TDK.Analytics.TrackCustomEvent("ftue_started", new Dictionary<string, object>
+        {
+            {"step", 0}
+        });
+    }
+
+    public void OnTrackFtueCompletedBtn()
+    {
+        TDK.Analytics.TrackCustomEvent("ftue_completed", new Dictionary<string, object>
+        {
+            {"step", 99}
+        });
+    }
+
+    public void OnTrackRewardClaimedBtn()
+    {
+        TDK.Analytics.TrackCustomEvent("reward_claimed", new Dictionary<string, object>
+        {
+            {"reward_id", "reward_001"}
+        });
+    }
+
     public void OnTrackCustomEventBtn()
     {
         TDK.Analytics.TrackCustomEvent("custom_event", new Dictionary<string, object>
