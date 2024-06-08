9a71984f863b7f308a1a416854f12257bd8b81c3
Marco Zakaria <marcozakaria@yahoo.com>
2024-06-08 22:57:25 +0300

    make header logo scale with image

--- Assets/Treasure/TDK/Runtime/Connect/UI/HeaderLogo.cs
+++ Assets/Treasure/TDK/Runtime/Connect/UI/HeaderLogo.cs
@@ -9,12 +9,19 @@ namespace Treasure
         [SerializeField] private AppSettingsData appSettingsData;
         [Space]
         [SerializeField] private Image iconImage;
+        [SerializeField] private AspectRatioFitter iconAspectRatioFitter;
+        [SerializeField] private LayoutElement logoLayoutElement;
         [SerializeField] private TMP_Text nameText;
 
         private void Start()
         {
             iconImage.sprite = appSettingsData.icon;
             nameText.text = appSettingsData.title;
+
+            var aspect = appSettingsData.icon.texture.width / (float) appSettingsData.icon.texture.height;
+            iconAspectRatioFitter.aspectRatio = aspect;
+            logoLayoutElement.preferredWidth = Mathf.Clamp(aspect * logoLayoutElement.preferredHeight, 
+                logoLayoutElement.preferredHeight, logoLayoutElement.preferredHeight * 2);
         }
     }
 }
