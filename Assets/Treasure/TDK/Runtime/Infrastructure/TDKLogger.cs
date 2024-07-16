4166a93dde0e2a1e66170bbef6232d0a457dc3e6
lefarchi <farchi@treasure.lol>
2024-07-16 16:15:34 -0300

    Refactor TDKLogger

--- Assets/Treasure/TDK/Runtime/Infrastructure/TDKLogger.cs
+++ Assets/Treasure/TDK/Runtime/Infrastructure/TDKLogger.cs
@@ -7,155 +7,63 @@ namespace Treasure
 {
     public static class TDKLogger
     {
-        public static bool verboseLogging = true; //TODO move to config driven?
+        public static Action<string> ExternalLogCallback;
 
-        public static Action<String> ExternalLogCallback;
-
-        public static void Log(string message, bool logOnMainThread=true)
+        public static void LogDebug(string message)
         {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.INFO) {
+            LogByLevel(TDKConfig.LoggerLevelValue.DEBUG, message);
+        }
+
+        public static void LogInfo(string message)
+        {
+            LogByLevel(TDKConfig.LoggerLevelValue.INFO, message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            LogByLevel(TDKConfig.LoggerLevelValue.WARNING, message);
+        }
+
+        public static void LogError(string message)
+        {
+            LogByLevel(TDKConfig.LoggerLevelValue.ERROR, message);
+        }
+
+        // Alias to LogInfo
+        public static void Log(string message)
+        {
+            LogInfo(message);
+        }
+
+        private static void LogByLevel(TDKConfig.LoggerLevelValue loggerLevelValue, string message) {
+            if (TDK.Instance.AppConfig.LoggerLevel > loggerLevelValue) {
                 return;
             }
+            TDKMainThreadDispatcher.Instance.Enqueue(() => ExternalLogCallback?.Invoke(message));
             #if UNITY_EDITOR
-            UnityEngine.Debug.Log(message);
-            TDKMainThreadDispatcher.Instance.Enqueue(() => ExternalLogCallback?.Invoke(message));
+            switch (loggerLevelValue)
+            {
+                case TDKConfig.LoggerLevelValue.ERROR:
+                    UnityEngine.Debug.LogError(message);
+                    break;
+                case TDKConfig.LoggerLevelValue.WARNING:
+                    UnityEngine.Debug.LogWarning(message);
+                    break;
+                default:
+                    UnityEngine.Debug.Log(message);
+                    break;
+            }
             #else
-            if(logOnMainThread)
-            {
-                TDKMainThreadDispatcher.Instance.Enqueue(LogCoroutine(message));
-            }
-            else
-            {
-                LogInternal(message);
-            }
+            TDKMainThreadDispatcher.Instance.Enqueue(() => LogMessageInternal(message));
             #endif
         }
 
-        private static IEnumerator LogCoroutine(string message)
+        private static void LogMessageInternal(string message)
         {
-            LogInternal(message);
-            yield return null;
-        }
-
-        private static void LogInternal(string message)
-        {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.DEBUG) {
-                return;
-            }
-            if(!string.IsNullOrEmpty(message))
-            {
-                try {
-                    #if UNITY_EDITOR
-                    StackFrame sf = new StackFrame(1);
-                    var method = sf.GetMethod();
-                    UnityEngine.Debug.Log(string.Format("[{0}] {1}:{2}", Time.realtimeSinceStartup, method.DeclaringType, message));
-                    #else
-                    UnityEngine.Debug.Log(string.Format("[{0}] {1}", Time.realtimeSinceStartup, message));
-                    #endif
-                    TDKMainThreadDispatcher.Instance.Enqueue(() => ExternalLogCallback?.Invoke(message));
-                }
-                catch
-                {
-                    // no-op
-                }
-            }
-        }
-
-        public static void LogWarning(string message, bool logOnMainThread=true)
-        {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.WARNING) {
-                return;
-            }
-            #if UNITY_EDITOR || TP_UA
-            UnityEngine.Debug.LogWarning(message);
-            TDKMainThreadDispatcher.Instance.Enqueue(() => ExternalLogCallback?.Invoke(message));
-            #else
-            if(logOnMainThread)
-            {
-                TDKMainThreadDispatcher.Instance.Enqueue(LogWarningCoroutine(message));
-            }
-            else
-            {
-                LogWarningInternal(message);
-            }
-            #endif
-        }
-
-        private static IEnumerator LogWarningCoroutine(string message)
-        {
-            LogWarningInternal(message);
-            yield return null;
-        }
-
-        private static void LogWarningInternal(string message)
-        {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.DEBUG) {
-                return;
-            }
-            if(!string.IsNullOrEmpty(message))
-            {
-                try {
-                    #if UNITY_EDITOR
-                    StackFrame sf = new StackFrame(1);
-                    var method = sf.GetMethod();
-                    UnityEngine.Debug.LogWarning(string.Format("[{0}] {1}:{2}", Time.realtimeSinceStartup, method.DeclaringType, message));
-                    #else
-                    UnityEngine.Debug.Log(string.Format("[{0}] {1}", Time.realtimeSinceStartup, message));
-                    #endif
-                }
-                catch
-                {
-                    // no-op
-                }
-            }
-        }
-
-        public static void LogError(string message, bool logOnMainThread=true)
-        {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.ERROR) {
-                return;
-            }
-            #if UNITY_EDITOR || TP_UA
-            UnityEngine.Debug.LogError(message);
-            TDKMainThreadDispatcher.Instance.Enqueue(() => ExternalLogCallback?.Invoke(message));
-            #else
-            if(logOnMainThread)
-            {
-                TDKMainThreadDispatcher.Instance.Enqueue(LogErrorCoroutine(message));
-            }
-            else
-            {
-                LogErrorInternal(message);
-            }
-            #endif
-        }
-
-        private static IEnumerator LogErrorCoroutine(string message)
-        {
-            LogErrorInternal(message);
-            yield return null;
-        }
-
-        private static void LogErrorInternal(string message)
-        {
-            if (TDK.Instance.AppConfig.LoggerLevel > TDKConfig.LoggerLevelValue.ERROR) {
-                return;
-            }
-            if(!string.IsNullOrEmpty(message))
-            {
-                try {
-                    #if UNITY_EDITOR
-                    StackFrame sf = new StackFrame(1);
-                    var method = sf.GetMethod();
-                    UnityEngine.Debug.LogError(string.Format("[{0}] {1}:{2}", Time.realtimeSinceStartup, method.DeclaringType, message));
-                    #else
-                    UnityEngine.Debug.Log(string.Format("[{0}] {1}", Time.realtimeSinceStartup, message));
-                    #endif
-                }
-                catch
-                {
-                    // no-op
-                }
+            try {
+                UnityEngine.Debug.Log(string.Format("[{0}] {1}", Time.realtimeSinceStartup, message));
+            } catch {
+                // no-op
             }
         }
     }
