6c9d4d9ee80d81ae63c8c62f709994303fcb7000
lefarchi <farchi@treasure.lol>
2024-08-29 20:36:33 -0300

    Remove merge markers. Remove duplicate code in analytics service. Remove old code

--- Assets/Treasure/TDK/Runtime/Infrastructure/TDKBaseService.cs
+++ Assets/Treasure/TDK/Runtime/Infrastructure/TDKBaseService.cs
@@ -6,8 +6,6 @@ namespace Treasure
     // why do we need empty game objects for services? it could be independent from unity
     public class TDKBaseService : MonoBehaviour
     {
-        protected bool appIsQuitting = false;
-
         public virtual void Awake()
         {
             DontDestroyOnLoad(this);
