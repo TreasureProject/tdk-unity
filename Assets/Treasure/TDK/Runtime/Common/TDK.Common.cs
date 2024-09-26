4d364685016b8f5704f5312f6f47752712c609cf
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-09-26 14:51:10 -0700

    update to wait on initial try

--- Assets/Treasure/TDK/Runtime/Common/TDK.Common.cs
+++ Assets/Treasure/TDK/Runtime/Common/TDK.Common.cs
@@ -30,17 +30,14 @@ namespace Treasure
             Transaction transaction;
             do
             {
-                if (retries > 0)
+                // equivalent to `await Task.Delay();` but works on webgl
+                var sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                while (sw.ElapsedMilliseconds < (retries == 0 ? initialWaitMs : retryMs))
                 {
-                    // equivalent to `await Task.Delay();` but works on webgl
-                    var sw = new System.Diagnostics.Stopwatch();
-                    sw.Start();
-                    while (sw.ElapsedMilliseconds < (retries == 0 ? initialWaitMs : retryMs))
-                    {
-                        await Task.Yield();
-                    }
-                    sw.Stop();
+                    await Task.Yield();
                 }
+                sw.Stop();
 
                 transaction = await TDK.API.GetTransactionByQueueId(queueId);
                 retries++;
