dea72129b05627091dfd8e80dc8515395dcc0530
lefarchi <farchi@treasure.lol>
2024-12-09 13:52:17 -0300

    DRY managed account factory constant

--- Assets/Treasure/TDK/Runtime/Common/Constants.cs
+++ Assets/Treasure/TDK/Runtime/Common/Constants.cs
@@ -17,7 +17,6 @@ namespace Treasure
     public enum Contract
     {
         Magic,
-        ManagedAccountFactory,
         MagicswapV2Router,
     }
 
@@ -29,47 +28,41 @@ namespace Treasure
         // misc values
         public const string SERVER_TIME_ENDPOINT_DEV = "https://darkmatter.spellcaster.lol/utils/time-unix"; //"https://trove-api.treasure.lol/v1/time";
         public const string SERVER_TIME_ENDPOINT_PROD = "https://darkmatter.treasure.lol/utils/time-unix"; //"https://trove-api.treasure.lol/v1/time";
+        public const string MANAGED_ACCOUNT_FACTORY_ADDRESS = "0x463effb51873c7720c810ac7fb2e145ec2f8cc60";
 
-        // contract accresses
+        // contract addresses
         public static Dictionary<ChainId, Dictionary<Contract, string>> ContractAddresses = new Dictionary<ChainId, Dictionary<Contract, string>> {
             {
                 ChainId.Arbitrum, new Dictionary<Contract, string> {
                     { Contract.Magic, "0x539bde0d7dbd336b79148aa742883198bbf60342" },
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" },
                     { Contract.MagicswapV2Router, "0xf7c8f888720d5af7c54dfc04afe876673d7f5f43" },
                 }
             },
             {
                 ChainId.ArbitrumSepolia, new Dictionary<Contract, string> {
                     { Contract.Magic, "0x55d0cf68a1afe0932aff6f36c87efa703508191c" },
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" },
                     { Contract.MagicswapV2Router, "0xa8654a8097b78daf740c1e2ada8a6bf3cd60da50" },
                 }
             },
             {
                 ChainId.Mainnet, new Dictionary<Contract, string> {
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" },
                 }
             },
             {
                 ChainId.Sepolia, new Dictionary<Contract, string> {
                     { Contract.Magic, "0x013cb2854daad8203c6686682f5d876e5d3de4a2" },
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" },
                 }
             },
             {
                 ChainId.Treasure, new Dictionary<Contract, string> {
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" }, // used for chain switching
                 }
             },
             {
                 ChainId.TreasureRuby, new Dictionary<Contract, string> {
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" },
                 }
             },
             {
                 ChainId.TreasureTopaz, new Dictionary<Contract, string> {
-                    { Contract.ManagedAccountFactory, "0x463effb51873c7720c810ac7fb2e145ec2f8cc60" }, // used for chain switching
                     { Contract.MagicswapV2Router, "0xad781ed13b5966e7c620b896b6340abb4dd2ca86" },
                 }
             }
