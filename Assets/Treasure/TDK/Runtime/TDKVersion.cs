d01f8fb8eb99634a0089d785b88adf0b4a93091a
lefarchi <farchi@treasure.lol>
2025-01-16 14:39:52 -0300

    Bump TDK version to 2.6.0

--- Assets/Treasure/TDK/Runtime/TDKVersion.cs
+++ Assets/Treasure/TDK/Runtime/TDKVersion.cs
@@ -3,6 +3,6 @@
         public static class TDKVersion
         {
                 public static string name = "tdk-unity";
-                public static string version = "2.5.0";
+                public static string version = "2.6.0";
         }
 }
